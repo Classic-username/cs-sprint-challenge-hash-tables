ae14b520ae49e8f666f33d3bf51f4e5a45e4eb0b
Joshua Draper <josh.drapergame@gmail.com>
2020-07-19 20:26:18 -0600

    finished ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,6 +4,28 @@ def intersection(arrays):
     """
     # Your code here
 
+    only_same = {}
+    everything = {}
+    length = len(arrays)
+
+    result = []
+
+
+
+    for array in arrays:
+        for i in array:
+            if i not in everything:
+                everything[i] = 1
+            else:
+                everything[i] += 1
+        
+    for i in everything:
+        if everything[i] == length:
+            only_same[i] = everything[i]
+
+    for key, value in only_same.items():
+        result.append(key)
+
     return result
 
 
